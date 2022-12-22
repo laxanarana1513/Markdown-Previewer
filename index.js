2ee69f36c9529e1cc8e2c978d2dea7310baa5f35
Laxana Rana <laxanarana1513@gmail.com>
2022-12-22 22:39:47 +0530

    Commit all

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log(marked.parse("hello program"));
+console.log()
+
+function modifyPreview() {
+    document.getElementById("preview").innerHTML = document.getElementById("editor").value
+}
+
